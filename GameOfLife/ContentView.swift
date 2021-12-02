07c2e7ef48e65deda4a5b42d6175263511614621
Kevin Hirsch <kevin.hirsch@immoweb.be>
2021-12-02 15:24:25 +0100

    Setup Xcode project

---
+++ GameOfLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameOfLife
+//
+//  Created by Kevin HIRSCH on 02/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Kevin HIRSCH on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
